e151ee8a48d91d5aa283f3725f60a760936084fc
GU-SHI-YU <gushiyu20001212@gmail.com>
2020-09-27 22:48:35 +0800

    2020.9.26

--- 2016-04-2.cpp
+++ 2016-04-2.cpp
@@ -1,63 +1,50 @@
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-int table[19][11];
-int block[5][5];
+int grid[19][10];
+int pattern[4][4];
 
 int main()
 {
     ios::sync_with_stdio(false);
-    for (int i = 1; i <= 15; i++)
+    int pos(0);
+    for (int i = 0; i < 150; i++)
+        cin >> grid[i / 10][i % 10];
+    for (int i = 0; i < 16; i++)
+        cin >> pattern[i / 4][i % 4];
+    cin >> pos;
+    pos -= 1;
+    int notNull;
+    for (int i = 3; i >= 0; i--)
     {
-        for (int j = 1; j <= 10; j++)
+        if (pattern[i][0] | pattern[i][1] | pattern[i][2] | pattern[i][3])
         {
-            cin >> table[i][j];
+            notNull = i;
+            break;
         }
     }
-    for (int j = 1; j <= 10; j++)
+    int row = 0;
+    while (1)
     {
-        table[16][j] = 1;
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                if ((row + notNull == 15) || (grid[row + i][pos + j] & pattern[i][j]))
+                    goto out;
+        ++row;
     }
-    for (int i = 1; i <= 4; i++)
+out:
+    row -= 1;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            grid[row + i][pos + j] |= pattern[i][j];
+
+    for (int i = 0; i < 15; i++)
     {
-        for (int j = 1; j <= 4; j++)
-        {
-            cin >> block[i][j];
-        }
-    }
-    int n;
-    cin >> n;
-    for (int i = 1; i <= 19; i++)
-    {
-        for (int j = 4; j >= 1; j--)
-        {
-            for (int k = 0; k < 4; k++)
-            {
-                if (block[j][k + 1] == 1 && table[i - 4 + j][k + n] == 1)
-                {
-                    for (int ii = 1; ii <= 4; ii++)
-                    {
-                        for (int jj = 1; jj <= 4; jj++)
-                        {
-                            if (i - 5 + ii >= 1 && block[ii][jj] == 1)
-                            {
-                                table[i - 5 + ii][jj + n - 1] = block[ii][jj];
-                            }
-                        }
-                    }
-                    for (int ii = 1; ii <= 15; ii++)
-                    {
-                        for (int jj = 1; jj <= 10; jj++)
-                        {
-                            cout << table[ii][jj] << " ";
-                        }
-                        cout << endl;
-                    }
-                    return 0;
-                }
-            }
-        }
+        for (int j = 0; j < 10; j++)
+            cout << grid[i][j] << " ";
+        cout << endl;
     }
     return 0;
 }
